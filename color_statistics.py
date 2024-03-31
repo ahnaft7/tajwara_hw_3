3a8a1befaa68c5950eeee6ba1edfb02239f38236
Ahnaf Tajwar <tajwara@bu.edu>
2024-03-31 01:04:48 -0400

    added data file

---
+++ color_statistics.py
@@ -0,0 +1,15 @@
+"""
+Ahnaf Tajwar
+Class: CS 677
+Date: 3/30/23
+Homework Problem # 1
+Description of Problem (just a 1-2 line summary!): This problem is to 
+"""
+
+import pandas as pd
+
+# read in text file
+df = pd.read_csv('data_banknote_authentication.txt', names=['f1', 'f2', 'f3', 'f4', 'class'])
+
+
+
